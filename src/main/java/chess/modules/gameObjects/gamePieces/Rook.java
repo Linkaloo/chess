b2b5b3df5fb6e3355187940fdf190e24a32ae2a8
Joshua <link7844@gmail.com>
2020-07-04 15:19:43 -0700

    Rook's legal moves implemented

--- src/main/java/chess/modules/gameObjects/gamePieces/Rook.java
+++ src/main/java/chess/modules/gameObjects/gamePieces/Rook.java
@@ -31,41 +31,76 @@ public class Rook extends Piece{
     @Override
     public List<PieceMove> getLegalMoves(Board board) {
         List<PieceMove> possibleMoves = getPossibleMoves(board);
+        List<PieceMove> legalMoves;
 
-        return possibleMoves
-                .stream()
-                .filter(pieceMove -> checkMove(pieceMove, board))
-                .collect(Collectors.toList());
-    }
+        Piece blockingPiece1 = null;
+        Piece blockingPiece2 = null;
+        Piece blockingPiece3 = null;
+        Piece blockingPiece4 = null;
 
-
-    private boolean checkMove(PieceMove pieceMove, Board board) {
-        boolean moveIsBlocked = false;
-        Piece boardPiece = board.getPieceOnBoard(pieceMove.getColumnPos(), pieceMove.getRowPos());
-        moveIsBlocked = checkBlocked(pieceMove, boardPiece);
-
-        if(boardPiece == null) {
-            return true;
+        for (int i = 0; i < possibleMoves.size(); i++) {
+            if (board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos()) != null && possibleMoves.get(i).getColumnPos() > columnPos) { // piece on left
+                blockingPiece1 = board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos());
+                break;
+            }
         }
-        else if(boardPiece.getPieceColor() != pieceColor)
-            return true;
-        else
-            return false;
+
+        for (int i = 0; i < possibleMoves.size(); i++) {
+            if (board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos()) != null && possibleMoves.get(i).getColumnPos() < columnPos) { // piece on right
+                blockingPiece2 = board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos());
+                break;
+            }
+        }
+
+        for (int i = 0; i < possibleMoves.size(); i++) {
+            if (board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos()) != null && possibleMoves.get(i).getRowPos() > rowPos) { // piece above
+                blockingPiece3 = board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos());
+                break;
+            }
+        }
+
+        for (int i = 0; i < possibleMoves.size(); i++) {
+            if (board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos()) != null && possibleMoves.get(i).getRowPos() < rowPos) { //piece below
+                blockingPiece4 = board.getPieceOnBoard(possibleMoves.get(i).getColumnPos(), possibleMoves.get(i).getRowPos());
+                break;
+            }
+        }
+
+        Piece finalBlockingPiece1 = blockingPiece1;
+        Piece finalBlockingPiece2 = blockingPiece2;
+        Piece finalBlockingPiece3 = blockingPiece3;
+        Piece finalBlockingPiece4 = blockingPiece4;
+        legalMoves = possibleMoves.stream().filter(pieceMove -> checkMove(pieceMove, finalBlockingPiece1, finalBlockingPiece2, finalBlockingPiece3, finalBlockingPiece4)).collect(Collectors.toList());
+
+        return legalMoves;
     }
 
-    private boolean checkBlocked(PieceMove pieceMove, Piece boardPiece) {
+    private boolean checkMove(PieceMove pieceMove, Piece finalBlockingPiece1, Piece finalBlockingPiece2, Piece finalBlockingPiece3, Piece finalBlockingPiece4) {
+        if (finalBlockingPiece1 != null && (pieceMove.getColumnPos() > finalBlockingPiece1.getColumnPos() ||
+                (pieceMove.getColumnPos() == finalBlockingPiece1.getColumnPos() && finalBlockingPiece1.getPieceColor().equals(pieceColor))))
+            return false;
+        if (finalBlockingPiece2 != null && (pieceMove.getColumnPos() < finalBlockingPiece2.getColumnPos() ||
+                (pieceMove.getColumnPos() == finalBlockingPiece2.getColumnPos() && finalBlockingPiece2.getPieceColor().equals(pieceColor))))
+            return false;
+        if (finalBlockingPiece3 != null && (pieceMove.getRowPos() > finalBlockingPiece3.getRowPos() ||
+                (pieceMove.getRowPos() == finalBlockingPiece3.getRowPos() && finalBlockingPiece3.getPieceColor().equals(pieceColor))))
+            return false;
+        if (finalBlockingPiece4 != null && (pieceMove.getRowPos() < finalBlockingPiece4.getRowPos() ||
+                (pieceMove.getRowPos() == finalBlockingPiece4.getRowPos() && finalBlockingPiece4.getPieceColor().equals(pieceColor))))
+            return false;
+
         return true;
     }
 
-    private List<PieceMove> getPossibleMoves(Board board) {
+    private List<PieceMove> getPossibleMoves (Board board){
         ArrayList<PieceMove> possibleMoves = new ArrayList<>();
 
-        for(int i = 1; i < 8; i++) {
+        for (int i = 1; i < 8; i++) {
             possibleMoves.add(new PieceMove(columnPos + i, rowPos));
             possibleMoves.add(new PieceMove(columnPos - i, rowPos));
 
             possibleMoves.add(new PieceMove(columnPos, rowPos + i));
-            possibleMoves.add(new PieceMove(columnPos , rowPos - i));
+            possibleMoves.add(new PieceMove(columnPos, rowPos - i));
         }
 
         return possibleMoves
