377c65c89c39ea282d1878b4d6d8cebdaf29a437
Joshua <link7844@gmail.com>
2020-07-05 22:54:34 -0700

    knight legal moves, pawn legal moves with partial enpasasant

--- src/main/java/chess/modules/gameObjects/gamePieces/Pawn.java
+++ src/main/java/chess/modules/gameObjects/gamePieces/Pawn.java
@@ -13,18 +13,22 @@ public class Pawn extends Piece {
 
     private boolean initialMove;
     private boolean promoted;
+    private boolean enpassantable;
 
     public Pawn(int columnPos, int rowPos, PieceColor pieceColor) {
         super(columnPos, rowPos, pieceColor);
         initialMove = true;
         promoted = false;
+        enpassantable = false;
 
         File file;
         switch (pieceColor) {
-            case BLACK: file = new File(String.valueOf(getClass().getClassLoader().getResource("assets/B.P.png")));
-            break;
-            case WHITE: file = new File(String.valueOf(getClass().getClassLoader().getResource("assets/W.P.png")));
-            break;
+            case BLACK:
+                file = new File(String.valueOf(getClass().getClassLoader().getResource("assets/B.P.png")));
+                break;
+            case WHITE:
+                file = new File(String.valueOf(getClass().getClassLoader().getResource("assets/W.P.png")));
+                break;
             default:
                 throw new IllegalStateException("Unexpected value: " + pieceColor);
         }
@@ -37,74 +41,94 @@ public class Pawn extends Piece {
         return promoted;
     }
 
+    public boolean isEnpassantable() {
+        return enpassantable;
+    }
+
     @Override
     public void move(PieceMove move) {
+        int currRow = rowPos;
         super.move(move);
         initialMove = false;
 
-        if(pieceColor == PieceColor.WHITE && rowPos == 0)
+        if (pieceColor == PieceColor.WHITE && rowPos + 2 == currRow)
+            enpassantable = true;
+        else if (pieceColor == PieceColor.BLACK && rowPos - 2 == currRow)
+            enpassantable = true;
+        else enpassantable = false;
+
+        if (pieceColor == PieceColor.WHITE && rowPos == 0)
             promoted = true;
         else promoted = pieceColor == PieceColor.BLACK && rowPos == 7;
     }
 
     @Override
     public List<PieceMove> getLegalMoves(Board board) {
-        List<PieceMove> possibleMoves = getPossibleMoves(board);
-
-        return possibleMoves
-                .stream()
-                .filter(pieceMove -> checkMove(pieceMove, board)).collect(Collectors.toList());
+        return getPossibleMoves(board);
     }
 
-    private boolean checkMove(PieceMove pieceMove, Board board) {
-        Piece boardPiece = board.getPieceOnBoard(pieceMove.getColumnPos(), pieceMove.getRowPos());
-        if(boardPiece == null)
-            return true;
-        else if(boardPiece != null && boardPiece.getPieceColor() != pieceColor)
-            return true;
-        else
-            return false;
+    private boolean moveIsBlocked(PieceMove testMove, Board board) {
+        return board.getPieceOnBoard(testMove.getColumnPos(), testMove.getRowPos()) != null;
     }
 
     private List<PieceMove> getPossibleMoves(Board board) {
-
         ArrayList<PieceMove> possibleMoves = new ArrayList<>();
 
-        //possibleMoves
-        if(pieceColor.equals(PieceColor.WHITE) && rowPos > 0) {
-            possibleMoves.add(new PieceMove(columnPos, rowPos - 1));
+        if (pieceColor.equals(PieceColor.WHITE) && rowPos > 0) {
+            Piece rightPiece = board.getPieceOnBoard(columnPos + 1, rowPos - 1);
+            Piece leftPiece = board.getPieceOnBoard(columnPos - 1, rowPos - 1);
 
-            if (initialMove) {
-                possibleMoves.add(new PieceMove(columnPos, rowPos - 2));
+            Piece rightPawn = board.getPieceOnBoard(columnPos + 1, rowPos);
+            Piece leftPawn = board.getPieceOnBoard(columnPos - 1, rowPos);
+
+            PieceMove testMove = new PieceMove(columnPos, rowPos - 1);
+            if(!moveIsBlocked(testMove, board))
+                possibleMoves.add(testMove);
+
+            if (initialMove && !moveIsBlocked(testMove, board)) {
+                testMove = new PieceMove(columnPos, rowPos - 2);
+                if(!moveIsBlocked(testMove, board))
+                    possibleMoves.add(testMove);
             }
 
-            if(columnPos != 0 && columnPos != 7) {
-                possibleMoves.add(new PieceMove(columnPos - 1, rowPos - 1));
+
+            if (columnPos >= 0 && rightPiece != null && !rightPiece.getPieceColor().equals(pieceColor) || (rightPawn instanceof Pawn && ((Pawn) rightPawn).isEnpassantable())) {
                 possibleMoves.add(new PieceMove(columnPos + 1, rowPos - 1));
             }
-            else if(columnPos == 0)
-                possibleMoves.add(new PieceMove(columnPos + 1, rowPos - 1));
-            else
+
+            if (columnPos <= 7 && leftPiece != null && !leftPiece.getPieceColor().equals(pieceColor) || (leftPawn instanceof Pawn && ((Pawn) leftPawn).isEnpassantable())) {
                 possibleMoves.add(new PieceMove(columnPos - 1, rowPos - 1));
-        }
-        else if (pieceColor.equals(PieceColor.BLACK) && rowPos < 7){
-            possibleMoves.add(new PieceMove(columnPos, rowPos + 1));
+            }
+        } else if (pieceColor.equals(PieceColor.BLACK) && rowPos < 7) {
+            Piece rightPiece = board.getPieceOnBoard(columnPos + 1, rowPos + 1);
+            Piece leftPiece = board.getPieceOnBoard(columnPos - 1, rowPos + 1);
+
+            Piece rightPawn = board.getPieceOnBoard(columnPos + 1, rowPos);
+            Piece leftPawn = board.getPieceOnBoard(columnPos - 1, rowPos);
+
+            PieceMove testMove = new PieceMove(columnPos, rowPos + 1);
+            if(!moveIsBlocked(testMove, board))
+                possibleMoves.add(testMove);
+
+            if (initialMove && !moveIsBlocked(testMove, board)) {
+                testMove = new PieceMove(columnPos, rowPos + 2);
+                if(!moveIsBlocked(testMove, board))
+                    possibleMoves.add(testMove);
+            }
 
             if (initialMove) {
                 possibleMoves.add(new PieceMove(columnPos, rowPos + 2));
             }
 
-            if(columnPos != 0 && columnPos != 7) {
-                possibleMoves.add(new PieceMove(columnPos - 1, rowPos + 1));
+            if (columnPos >= 0 && rightPiece != null && !rightPiece.getPieceColor().equals(pieceColor) || (rightPawn instanceof Pawn && ((Pawn) rightPawn).isEnpassantable())) {
                 possibleMoves.add(new PieceMove(columnPos + 1, rowPos + 1));
             }
-            else if(columnPos == 0)
-                possibleMoves.add(new PieceMove(columnPos + 1, rowPos + 1));
-            else
-                possibleMoves.add(new PieceMove(columnPos - 1, rowPos + 1));
-        }
 
+            if (columnPos <= 7 && leftPiece != null && !leftPiece.getPieceColor().equals(pieceColor) || (leftPawn instanceof Pawn && ((Pawn) leftPawn).isEnpassantable())) {
+                possibleMoves.add(new PieceMove(columnPos - 1, rowPos + 1));
+            }
+        }
         return possibleMoves;
     }
-
 }
+
